1e36259164aa983d1870087d71362f4b01b08225
harjeeyborlah <harjeeyborlah@gmail.com>
2023-04-03 12:03:05 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        display:['Cinzel Decorative', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
+
