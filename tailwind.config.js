48954fced808e4a67eb2c5f1c2d935f1a044cbc2
harjeeyborlah <harjeeyborlah@gmail.com>
2023-04-04 12:44:41 +0100

    first commit of 4/3/2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","index.html"],
+  content: ["./src/**/*.{html,js}","index.html","about.html"],
   theme: {
     extend: {
       fontFamily:{
