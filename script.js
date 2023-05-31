9e9cf839b5383dae9f90ba29f86662a84f2f2bf5
harjeeyborlah <harjeeyborlah@gmail.com>
2023-05-31 16:52:35 +0100

    31/5/2023

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+var textarea = document.getElementById("my-textarea");
+  var charCount = document.getElementById("char-count");
+
+  textarea.addEventListener("input", function() {
+    var remainingChars = 75 - textarea.value.length;
+    charCount.textContent = remainingChars;
+  });
+  
+$('#mySelect').on('change', function() {
+  var selectedOption = $(this).val();
+  $(this).find('option[value="' + selectedOption + '"]').attr('selected', true);
+});
+
+
+var checkbox = document.getElementById("gift-wrap");
+  var label = document.getElementById("gift-wrap-label");
+
+  checkbox.addEventListener("change", function() {
+    label.classList.toggle("font-bold", checkbox.checked);
+  });
+
+
 const scrollToTopBtn = document.getElementById("scroll-to-top");
 
 window.addEventListener("scroll", () => {
@@ -14,3 +36,7 @@ scrollToTopBtn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+
+
+
